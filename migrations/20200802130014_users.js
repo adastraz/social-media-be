d552a090284f03e100f91680f353108b698b8386
adastraz <tylergosox@gmail.com>
2020-08-02 13:51:57 -0400

    completed the first iteration of user table
    eventually some of these fields will have to be seperate tables to allow for mutliple entries

--- migrations/20200802130014_users.js
+++ migrations/20200802130014_users.js
@@ -1,8 +1,30 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema
+        .createTable('users', tbl => {
+            tbl.increments()
+            tbl.string('username', 255)
+                .notNullable()
+                .unique()
+            tbl.string('password', 255)
+                .notNullable()
+            tbl.string('bio')
+            tbl.string('profileimg')
+            tbl.string('coverimg')
+            tbl.string('relationship')
+            tbl.date('birthday')
+                .notNullable()
+            tbl.timestamp('created_at')
+                .defaultTo(knex.fn.now())
+            tbl.string('phone_number', 15)
+            tbl.string('nickname')
+            tbl.string('location')
+            tbl.string('workplace')
+            tbl.string('education')
+        })
 }
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('users')
 }
