4c93c22f6ffd39d34d4f5c091db0aafc683be535
adastraz <tylergosox@gmail.com>
2020-08-02 16:25:22 -0400

    user model / router almost complete
    going to move on for now - would like to get some posts possibly!

---
+++ users/users-router.js
@@ -0,0 +1,80 @@
+const router = require('express').Router()
+
+const Users = require('./users-model.js')
+
+router.get('/', (req, res) => {
+    Users.find()
+        .then(users => res.json(users))
+        .catch(err => res.send(err))
+})
+
+router.get('/:id', idUser, (req, res) => {
+    const { id } = req.params
+
+    Users.findById(id)
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(404).json({ message: 'Could not find the user with given id.' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Failed to get user' })
+        })
+})
+
+router.put('/:id', idUser, (req, res) => {
+    const { id } = req.params
+
+    Users.findById(id)
+        .then(user => {
+            if (user) {
+                Users.update(id, req.body)
+                    .then(updatedUser => {
+                        res.json(updatedUser)
+                    })
+                    .catch(err => res.status(401).json({ message: 'Failed to update user', err}))
+            } else {
+                res.status(404).json({ message: 'Could not find user with given id' })
+            }
+        })
+        .catch (err => {
+            console.log(err)
+            res.status(500).json({ message: 'Invalid user' })
+        })
+})
+
+
+router.delete('/:id', idUser, (req, res) => {
+    const { id } = req.params
+
+    Users.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.json({ removed: deleted })
+            } else {
+                res.status(404).json({ message: 'Could not find user with given id' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Failed to delete user' })
+        })
+})
+
+function idUser (req, res, next) {
+    const { id } = req.params
+
+    Users.findById(id)
+        .then(result => {
+            if(result && Object.entries(result).length){
+                next()
+            }else {
+                res.status(400).json({ message: 'User does not exist' })
+            }
+        })
+}
+
+module.exports = router
